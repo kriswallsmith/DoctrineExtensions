912789d3c97ae5e7809900b2f5a1150f781e5a7d
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-06-12 13:36:26 -0400

    add ability to configure managers, make test pass

--- lib/Gedmo/References/ReferencesListener.php
+++ lib/Gedmo/References/ReferencesListener.php
@@ -2,13 +2,23 @@
 
 namespace Gedmo\References;
 
-use Doctrine\ODM\MongoDB\DocumentManager;
-
 use Doctrine\Common\EventArgs;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\Mapping\ClassMetadata as ORMClassMetadata;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata as MongoDBClassMetadata;
+use Gedmo\Exception\InvalidArgumentException;
 use Gedmo\Mapping\MappedEventSubscriber;
 
 class ReferencesListener extends MappedEventSubscriber
 {
+    private $managers;
+
+    public function __construct(array $managers = array()) {
+        $this->managers = $managers;
+    }
+
     public function loadClassMetadata(EventArgs $eventArgs)
     {
         $ea = $this->getEventAdapter($eventArgs);
@@ -25,9 +35,23 @@ class ReferencesListener extends MappedEventSubscriber
         $ea = $this->getEventAdapter($eventArgs);
         $om = $ea->getObjectManager();
         $uow = $om->getUnitOfWork();
-        $object = $om instanceof DocumentManager ? $eventArgs->getDocument() : $eventArgs->getEntity();
+        $object = $ea->getObject();
         $meta = $om->getClassMetadata(get_class($object));
-        var_dump($this->getConfiguration($om, $meta->name));
+        $config = $this->getConfiguration($om, $meta->name);
+        foreach ($config['referenceOne'] as $mapping) {
+            if (isset($mapping['identifier'])) {
+                $property = $meta->reflClass->getProperty($mapping['field']);
+                $property->setAccessible(true);
+                $meta->setFieldValue(
+                    $object,
+                    $mapping['identifier'],
+                    $this->extractIdentifier(
+                        $this->managers[$mapping['type']],
+                        $property->getValue($object)
+                    )
+                );
+            }
+        }
     }
 
     public function preUpdate(EventArgs $eventArgs)
@@ -45,8 +69,61 @@ class ReferencesListener extends MappedEventSubscriber
         );
     }
 
+    public function registerManager($type, ObjectManager $manager)
+    {
+        $this->managers[$type] = $manager;
+    }
+
     protected function getNamespace()
     {
         return __NAMESPACE__;
+    }
+
+    /**
+     * TODO: put this method into appropriate driver
+     *
+     * Extracts identifiers from object or proxy
+     *
+     * @param DocumentManager $dm
+     * @param object $object
+     * @param bool $single
+     * @return mixed - array or single identifier
+     */
+    private function extractIdentifier(ObjectManager $om, $object, $single = true)
+    {
+        if ($om instanceof DocumentManager) {
+            $meta = $om->getClassMetadata(get_class($object));
+            if ($object instanceof Proxy) {
+                $id = $om->getUnitOfWork()->getDocumentIdentifier($object);
+            } else {
+                $id = $meta->getReflectionProperty($meta->identifier)->getValue($object);
+            }
+
+            if ($single || !$id) {
+                return $id;
+            } else {
+                return array($meta->identifier => $id);
+            }
+        }
+        if ($om instanceof EntityManager) {
+            if ($object instanceof Proxy) {
+                $id = $om->getUnitOfWork()->getEntityIdentifier($object);
+            } else {
+                $meta = $om->getClassMetadata(get_class($object));
+                $id = array();
+                foreach ($meta->identifier as $name) {
+                    $id[$name] = $meta->getReflectionProperty($name)->getValue($object);
+                    // return null if one of identifiers is missing
+                    if (!$id[$name]) {
+                        return null;
+                    }
+                }
+            }
+
+            if ($single) {
+                $id = current($id);
+            }
+            return $id;
+        }
     }
 }
